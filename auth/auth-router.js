eedee22fb5436de908db3756b213576b0901abad
James Pak <jamespak@Josephs-MacBook-Air.local>
2019-06-24 12:44:34 -0700

    register endpoint for adding user set up

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,3 +5,20 @@ const jwt = require('jsonwebtoken');
 const Users = require('../users/users-model');
 const secrets = require('../config/secrets');
 
+router.post('/register', (req ,res) => {
+    let user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 10);
+
+    user.password = hash;
+
+    Users.add(user)
+    .then(saved => {
+        const token = generateToken(user);
+
+        res.status(201).json({ token });
+    })
+    .catch(error => {
+        res.status(500).json(error.message)
+    })
+})
