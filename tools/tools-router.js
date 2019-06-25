f4daa1ea132d85f558fb20c77942a0168acf993a
James Pak <jamespak@Josephs-MacBook-Air.local>
2019-06-25 01:13:53 -0700

    crud post and put set up in tools router

--- tools/tools-router.js
+++ tools/tools-router.js
@@ -13,4 +13,34 @@ router.get('/', (req, res) => {
     })
 }) 
 
+router.post('/', (req, res) => {
+    Tools.add(req.body, 'id')
+    .then(tool => {
+        if (tool) {
+            res.status(201).json(tool)
+        }
+        else {
+            res.status(422).json({ message: 'Incomplete entry' })
+        }
+    })
+    .catch(error => {
+        res.status(500).json(error)
+    })
+})
+
+router.put('/:id', async (req ,res) => {
+    try {
+        const tool = await debug.update(req.params.id, req.body);
+        if (tool) {
+            res.status(200).json(tool)
+        }
+        else {
+            res.status(404).json({ message: 'Tool could not be found!' })
+        }
+    }
+    catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 module.exports = router;
