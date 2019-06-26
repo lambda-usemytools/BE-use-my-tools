6c4583054ffc830874a6e22835528b56fb1b4a8e
James Pak <jamespak@Josephs-MacBook-Air.local>
2019-06-26 14:47:09 -0700

    get and post for users router tested

--- users/users-model.spec.js
+++ users/users-model.spec.js
@@ -1,6 +1,6 @@
 const db = require('../data/dbConfig');
 
-const { add, find, } = require('./users-model');
+const { add, find, findById } = require('./users-model');
 
 describe('users model', () => {
     beforeEach(async () => {
@@ -26,6 +26,28 @@ describe('users model', () => {
 
             expect(users).toEqual([]);
         })
+    })
+
+    describe('findById()', () => {
+        it('should return a specified user', async () => {
+            
+            const user = await add({ first_name: "Bot", last_name: "One", email: "test@gmail.com", password: "pass" })
+
+            const result = await findById(1)
+
+            expect(user.first_name).toBe(result.first_name)
+        })
+    })
+
+    describe('add()', () => {
+        it('should insert the provided user', async () => {
+            await add({ first_name: "Bot", last_name: "One", email: "test@gmail.com", password: "pass" })
+
+            const users = await db('users');
+
+            expect(users).toHaveLength(1)
+        })
+
     })
 
 
---
+++ users/users-router.spec.js
@@ -0,0 +1,36 @@
+const supertest = require('supertest');
+const router = require('./users-routers');
+
+describe('users router', () => {
+    describe('GET /', () => {
+
+        it('responds with 200 OK', () => {
+            supertest(router).get('/users').expect(200)
+        })
+
+        it('responds with 200 OK', () => {
+            supertest(router).get('/users').expect('Content-Type', /json/i)
+        })
+
+        it('responds wtih 500 ERROR', () => {
+            supertest(router).get('/userz').expect(500)
+        })
+    })
+
+    describe('POST /', () => {
+
+        it('responds with 200 OK', () => {
+            supertest(router).post('/users').expect(200);
+        })
+
+        it('responds with 200 OK', () => {
+            supertest(router).post('/users').expect('Content-Type', /json/i)
+        })
+
+        it('responds with 500 ERROR', () => {
+            supertest(router).post('/userz').expect(500)
+        })
+        
+    })
+
+})
