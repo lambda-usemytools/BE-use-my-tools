dd0435536b421d27b563d1f62d164ea176187154
James Pak <jamespak@Josephs-MacBook-Air.local>
2019-06-26 22:35:20 -0700

    changed subject to in payload

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -45,7 +45,7 @@ router.post('/login', (req, res) => {
 
 function generateToken(user) {
     const payload = {
-        subject: user.id,
+        id: user.id,
         first_name: user.first_name,
     }
 
